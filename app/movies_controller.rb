e615758007574d91a5cc4fb82301190650292f15
Brandon Weaver <a7xncob@gmail.com>
2019-12-13 20:23:53 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,6 +29,12 @@ def can_be_created_in_a_block(args = nil)
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
+  if args = nil
+    args = {
+      title: "Home Alone",
+      release_date: 1990
+    }
+  end
   
   Movie.create do |m|
     m.save(args)
