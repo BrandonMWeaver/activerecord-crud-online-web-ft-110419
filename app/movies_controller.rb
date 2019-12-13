8d4c5001910b82639d67114bc5399750804fd874
Brandon Weaver <a7xncob@gmail.com>
2019-12-13 21:32:57 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -4,7 +4,7 @@
 # def make_a_new_movie_instance    # def make_a_new_movie_instance
 #   movie = __                     #   movie = Movie.new
 # end                              # end
-require "pry"
+
 def can_be_instantiated_and_then_saved
   movie = Movie.new
   movie.title = "This is a title."
@@ -39,8 +39,7 @@ def can_be_created_in_a_block(args = nil)
   
   Movie.create do |m|
     m = Movie.new(args)
-    binding.pry
-  end
+  end.first
 end
 
 def can_get_the_first_item_in_the_database
